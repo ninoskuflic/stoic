7375e8d18d04a975412724d27360011a2f34bb76
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2024-02-21 20:01:26 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
-import Loading from './loading.svg'
+import Loading from './loading.svg';
+
 export default function App() {
   // API State
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  const [quotes, setQuotes] = useState([]);
+  const [currentQuote, setCurrentQuote] = useState(null);
 
-  // Function to fetch data based on ID
-  const fetchData = async (id) => {
+  // Function to fetch all quotes
+  const fetchAllQuotes = async () => {
     setLoading(true);
     setError(null);
     try {
-      const response = await fetch(`https://api.learn.skuflic.com/stoic/${id}`);
+      const response = await fetch('https://api.learn.skuflic.com/stoic');
       const result = await response.json();
-      setData(result);
+      setQuotes(result);
     } catch (error) {
       setError(error);
     } finally {
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
     }
-  }
+  };
 
   // Function to fetch new random quote
   const fetchRandomQuote = () => {
-    const randomId = Math.floor(Math.random() * 100) + 1; // Generate random ID between 1 and 100
-    fetchData(randomId);
-  }
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setCurrentQuote(quotes[randomIndex]);
+  };
 
-  // Fetch random quote on component mount
+  // Fetch all quotes on component mount
   useEffect(() => {
-    fetchRandomQuote();
+    fetchAllQuotes();
   }, []);
+
+  // Fetch random quote on initial load
+  useEffect(() => {
+    if (quotes.length > 0) {
+      fetchRandomQuote();
+    }
+  }, [quotes]);
 
   return (
     <>
-      {loading ? (
-        <div className='loading'><img src={Loading} alt='Loading' /></div>
-      ) : error ? (
+      {error ? (
         <div className='error'>
           <h1>Whoopsy daisy</h1>
           <p>An unexpected error occurred, please contact support and provide this error message — {error.message.toLowerCase()}.</p>
         </div>
       ) : (
         <>
-          <main>
-            <h1>'{data?.quote}'</h1>
-            <p>— {data?.author}, {data?.year}</p>
-          </main >
+          {
+            loading ? (
+              <div className='loading'><img src={Loading} alt='Loading' /></div>
+            ) :
+              <main>
+                <h1>'{currentQuote?.quote}'</h1>
+                <p>— {currentQuote?.author}, {currentQuote?.year}</p>
+              </main >
+          }
+
           <footer>
             <button onClick={fetchRandomQuote} alt='refresh'>
               <span className='material-symbols-outlined'>
@@ -60,7 +75,7 @@ export default function App() {
                 <a href='https://go.skuflic.com/servicesagreement' target='_blank' rel='noreferrer'>Services Agreement</a>
                 <a href='https://go.skuflic.com/privacy' target='_blank' rel='noreferrer'>Privacy Policy</a>
               </div>
-              <p>TM and Copyright &copy; {new Date().getFullYear()} Skuflic.com. All rights reserved. Quotes provided are AI-generated for inspirational purposes only.</p>
+              <p>TM and Copyright &copy; {new Date().getFullYear()} Skuflic.com. All rights reserved. The quotes are AI-generated and for inspirational purposes only. While efforts have been made to ensure the accuracy and appropriateness of the content, it's important to recognize that AI-generated text may occasionally contain inaccuracies, offensive language, or incorrect information.</p>
             </div>
           </footer>
         </>
