9b161be90dc13ab3816cc97e12c89f91da07f446
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2024-02-23 19:02:37 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,143 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 import Loading from './loading.svg';
+import pluralize from 'pluralize';
 
 export default function App() {
   // API State
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [quotes, setQuotes] = useState([]);
-  const [currentQuote, setCurrentQuote] = useState(null);
+  const [data, setData] = useState(null);
+  const [timeRemaining, setTimeRemaining] = useState(0); // State to hold remaining time until next quote refresh
 
-  // Function to fetch all quotes
-  const fetchAllQuotes = async () => {
+  const storecurrentQuoteInLocalStorage = (quoteObject) => {
+    const currentTime = Math.floor(Date.now() / 1000); // Get current Unix timestamp in seconds
+    const dataToStore = {
+      id: quoteObject.id,
+      quote: quoteObject.quote,
+      author: quoteObject.author,
+      year: quoteObject.year,
+      timestamp: currentTime,
+    };
+    localStorage.setItem('currentQuote', JSON.stringify(dataToStore));
+  };
+
+
+  // Function to fetch data based on ID
+  const fetchData = async (id) => {
     setLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://api.learn.skuflic.com/stoic');
+      const response = await fetch(`https://api.learn.skuflic.com/stoic/${id}`);
       const result = await response.json();
-      setQuotes(result);
+      setData(result);
+      storecurrentQuoteInLocalStorage(result);
     } catch (error) {
       setError(error);
     } finally {
+
       setTimeout(() => {
         setLoading(false);
-      }, 500);
+      }, '500');
     }
-  };
+  }
 
   // Function to fetch new random quote
   const fetchRandomQuote = () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    setCurrentQuote(quotes[randomIndex]);
+    let shownQuotes = JSON.parse(localStorage.getItem('shownQuotes')) || [];
+
+    const generateRandomId = () => Math.floor(Math.random() * 100) + 1;
+
+    let randomId = generateRandomId();
+
+    // Check if randomId already exists in shownQuotes
+    while (shownQuotes.includes(randomId)) {
+      randomId = generateRandomId();
+    }
+
+    // If shownQuotes length exceeds 90, remove the oldest item
+    if (shownQuotes.length >= 90) {
+      shownQuotes.shift(); // Remove the oldest item
+    }
+
+    fetchData(randomId);
+
+    // Update shownQuotes in localStorage
+    shownQuotes.push(randomId);
+    localStorage.setItem('shownQuotes', JSON.stringify(shownQuotes));
+  }
+
+
+  const isQuoteOlderThan24Hours = (storedTimestamp) => {
+    if (!storedTimestamp) return true; // If there's no timestamp, consider it older
+    const currentTime = new Date().getTime(); // Current time in milliseconds
+    const storedTime = storedTimestamp * 1000; // Convert stored timestamp from seconds to milliseconds
+    const millisecondsPerHour = 3600 * 1000; // 1 hour in milliseconds
+    const hoursPerDay = 24;
+    const millisecondsPerDay = hoursPerDay * millisecondsPerHour; // 24 hours in milliseconds
+    return (currentTime - storedTime) >= millisecondsPerDay;
   };
 
-  // Fetch all quotes on component mount
+
+  // Function to calculate remaining time until next quote refresh
+  const calculateTimeRemaining = () => {
+    const storedQuote = JSON.parse(localStorage.getItem('currentQuote'));
+    if (storedQuote) {
+      const currentTime = Math.floor(Date.now() / 1000); // Current Unix timestamp in seconds
+      const nextRefreshTimestamp = storedQuote.timestamp + 24 * 60 * 60; // Timestamp for next refresh (24 hours later)
+      const remainingTime = nextRefreshTimestamp - currentTime;
+      setTimeRemaining(remainingTime > 0 ? remainingTime : 0);
+    }
+  };
+
+
+  const formatTimeRemaining = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+    return `
+    
+    ${hours} ${pluralize('hour', hours)}
+    
+    ${minutes} ${pluralize('minute', minutes)}
+    
+    ${seconds} ${pluralize('second', seconds)}`;
+  };
+
+  // Fetch random quote on component mount
+  const [isInitialRender, setIsInitialRender] = useState(true);
+
   useEffect(() => {
-    fetchAllQuotes();
+    const storedQuote = JSON.parse(localStorage.getItem('currentQuote'));
+
+    if (storedQuote == null) {
+      fetchRandomQuote();
+    } else if (isQuoteOlderThan24Hours(storedQuote.timestamp)) {
+      fetchRandomQuote();
+    } else {
+      setData(storedQuote);
+      setLoading(false);
+    }
+
+    // Calculate time remaining on component mount
+    calculateTimeRemaining();
+
+    // Update time remaining every second
+    const timer = setInterval(() => {
+      calculateTimeRemaining();
+    }, 1000);
+
+    return () => clearInterval(timer); // Cleanup interval on unmount
   }, []);
 
-  // Fetch random quote on initial load
   useEffect(() => {
-    if (quotes.length > 0) {
+    if (!isInitialRender && timeRemaining === 0) {
       fetchRandomQuote();
+    } else {
+      setIsInitialRender(false);
     }
-  }, [quotes]);
+  }, [timeRemaining]);
+
 
   return (
     <>
@@ -58,24 +153,22 @@ export default function App() {
               <div className='loading'><img src={Loading} alt='Loading' /></div>
             ) :
               <main>
-                <h1>'{currentQuote?.quote}'</h1>
-                <p>— {currentQuote?.author}, {currentQuote?.year}</p>
-              </main >
+                <p className='next-qoute'>Next quote available in {formatTimeRemaining(timeRemaining)}</p>
+                <h1>'{data?.quote}'</h1>
+                <p>— {data?.author}, {data?.year}</p>
+              </main>
           }
 
           <footer>
-            <button onClick={fetchRandomQuote} alt='refresh'>
-              <span className='material-symbols-outlined'>
-                refresh
-              </span>
-            </button>
             <div className='legal'>
               <div>
                 <a href='https://go.skuflic.com/terms' target='_blank' rel='noreferrer'>Terms of Use</a>
                 <a href='https://go.skuflic.com/servicesagreement' target='_blank' rel='noreferrer'>Services Agreement</a>
                 <a href='https://go.skuflic.com/privacy' target='_blank' rel='noreferrer'>Privacy Policy</a>
+                <a href='https://go.skuflic.com/report-a-bug' target='_blank' rel='noreferrer'>Bug Report</a>
+                <a href='https://go.skuflic.com/status' target='_blank' rel='noreferrer'>System Status</a>
               </div>
-              <p>TM and Copyright &copy; {new Date().getFullYear()} Skuflic.com. All rights reserved. The quotes are AI-generated and for inspirational purposes only. While efforts have been made to ensure the accuracy and appropriateness of the content, it's important to recognize that AI-generated text may occasionally contain inaccuracies, offensive language, or incorrect information.</p>
+              <p>TM and Copyright &copy; {new Date().getFullYear()} Skuflic.com. All rights reserved.</p>
             </div>
           </footer>
         </>
--- src/index.js
+++ src/index.js
@@ -2,28 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.createElement('div');
-rootElement.id = 'react-chrome-app';
-
-const globalStyles = document.createElement('style');
-globalStyles.innerHTML = `
-  #${rootElement.id} {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 300px;
-  height: 100vh;
-  background: #ffffff;
-  border-right: 1px solid #c2c2c2;
-  z-index: 999;
-  }
-`;
-rootElement.appendChild(globalStyles);
-document.body.appendChild(rootElement);
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
